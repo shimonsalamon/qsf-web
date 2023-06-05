51301a7b71a10cdd5fc3457808d1c591a030655b
Shimon Salamon <shimon8144@gmail.com>
2023-06-05 14:45:47 -0400

    updated

--- script.js
+++ script.js
@@ -4,9 +4,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var menuToggle = document.querySelector('.menu-toggle');
     var menu = document.querySelector('.menu');
-    const year = new Date().getFullYear();
-    
-    document.getElementById("year").textContent = year;
   
     menuToggle.addEventListener('click', function() {
       menu.classList.toggle('show');
